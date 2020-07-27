ad602bf3697556c073affadabea363f9d3f1e149
JUAN PABLO PIZARRO <juanpablopizarro97@gmail.com>
2020-07-27 15:37:51 -0500

    MONGO environment Variable

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = "mongodb://localhost:27017/cafe"
 }else{
-    urlDB = "mongodb+srv://pizarro:5cQ4LJVNdMn4qTi6@cluster0.mtgpg.mongodb.net/cafe"
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
